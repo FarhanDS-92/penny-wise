63f6cb5a9b5c30c9ec04c9bd63928cd60be2bf89
Vicpr22 <vicpr22@hotmail.com>
2024-01-11 15:47:37 -0500

    budget-css

--- src/app/(pages)/budget/page.jsx
+++ src/app/(pages)/budget/page.jsx
@@ -47,23 +47,30 @@ export default async function budget() {
   }
 
   return (
-    <section>
+    <section className="budget-section">
       <Link href={"/capital"}>
-        <div id="totalCapital">
+        <div id="totalCapital" className="budget-item">
           <h2>Capital</h2>
           <p>{totalCapital(capital)}</p>
         </div>
       </Link>
 
       <Link href={"/expense"}>
-        <div id="totalExpense">
+        <div id="totalExpense" className="budget-item">
           <h2>Expenses</h2>
           <p>{totalExpenses(expenses)}</p>
         </div>
       </Link>
 
-      <div id="budgetAllocation">
-        <h2>{checkSurplus(isSurplus) ? "Surplus" : "Deficit"}</h2>
+      <div id="budgetAllocation" className="budget-allocation">
+        <h2
+          style={{
+            fontSize: "1.5em",
+            color: isSurplus >= 0 ? "#27ae60" : "#c0392b",
+          }}
+        >
+          {checkSurplus(isSurplus) ? "Surplus" : "Deficit"}
+        </h2>
         <p>{isSurplus}</p>
       </div>
     </section>
