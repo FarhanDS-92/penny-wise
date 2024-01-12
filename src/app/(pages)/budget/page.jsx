20c025d90815b6ea61ea26dc881ddbb4c094396e
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-01-11 23:27:13 -0500

    finished logic of budget page overview

--- src/app/(pages)/budget/page.jsx
+++ src/app/(pages)/budget/page.jsx
@@ -5,74 +5,43 @@ import Link from "next/link.js";
 export default async function budget() {
   const user = await fetchUser();
 
-  const capital = await prisma.capital.findMany({
-    where: {
-      userId: user.userId,
-    },
-  });
-
-  const expenses = await prisma.expense.findMany({
+  const budgets = await prisma.budget.findMany({
     where: {
       userId: user.id,
     },
+    include: {
+      expenses: true,
+      goals: true,
+      capital: true,
+      categories: {
+        include: {
+          expenses: true,
+        },
+      },
+    },
+    orderBy: [
+      {
+        year: "asc",
+      },
+      {
+        month: "asc",
+      },
+    ],
   });
 
-  function totalCapital(capital) {
-    let totalCash = 0;
+  // this separates the budget array to be split by years and group them ot its own array
+  const separatedArrays = [];
+  budgets.forEach((obj) => {
+    const year = obj.year;
 
-    for (let i = 0; i < capital.length; i++) {
-      totalCash += capital[i].amount;
+    const index = separatedArrays.findIndex((arr) => arr[0].year === year);
+
+    if (index === -1) {
+      separatedArrays.push([obj]);
+    } else {
+      separatedArrays[index].push(obj);
     }
+  });
 
-    return totalCash;
-  }
-
-  function totalExpenses(expenses) {
-    let totalExpense = 0;
-
-    for (let i = 0; i < expenses.length; i++) {
-      totalExpense += expenses[i].cost;
-    }
-
-    return totalExpense;
-  }
-
-  const isSurplus = totalCapital(capital) - totalExpenses(expenses);
-
-  function checkSurplus(isSurplus) {
-    if (isSurplus >= 0) {
-      return true;
-    }
-    return false;
-  }
-
-  return (
-    <section className="budget-section">
-      <Link href={"/capital"}>
-        <div id="totalCapital" className="budget-item">
-          <h2>Capital</h2>
-          <p>{totalCapital(capital)}</p>
-        </div>
-      </Link>
-
-      <Link href={"/expense"}>
-        <div id="totalExpense" className="budget-item">
-          <h2>Expenses</h2>
-          <p>{totalExpenses(expenses)}</p>
-        </div>
-      </Link>
-
-      <div id="budgetAllocation" className="budget-allocation">
-        <h2
-          style={{
-            fontSize: "1.5em",
-            color: isSurplus >= 0 ? "#27ae60" : "#c0392b",
-          }}
-        >
-          {checkSurplus(isSurplus) ? "Surplus" : "Deficit"}
-        </h2>
-        <p>{isSurplus}</p>
-      </div>
-    </section>
-  );
+  return <section className="budget-section"></section>;
 }
