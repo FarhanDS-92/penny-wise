16c5f4596882b3032cd06ed8e2363cf3b0c96e62
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-01-14 15:41:37 -0500

    pull request from pimpa

--- src/app/(pages)/budget/page.jsx
+++ src/app/(pages)/budget/page.jsx
@@ -4,38 +4,8 @@ import { prisma } from "@/lib/prisma.js";
 import Link from "next/link.js";
 
 export default async function budget() {
-	const user = await fetchUser();
+  const user = await fetchUser();
 
-<<<<<<< HEAD
-	const capital = await prisma.capital.findMany({
-		where: {
-			userId: user.userId,
-		},
-	});
-
-	const expenses = await prisma.expense.findMany({
-		where: {
-			userId: user.id,
-		},
-	});
-
-	function totalCapital(capital) {
-		let totalCash = 0;
-
-		for (let i = 0; i < capital.length; i++) {
-			totalCash += capital[i].amount;
-		}
-
-		return totalCash;
-	}
-
-	function totalExpenses(expenses) {
-		let totalExpense = 0;
-
-		for (let i = 0; i < expenses.length; i++) {
-			totalExpense += expenses[i].cost;
-		}
-=======
   const budgets = await prisma.budget.findMany({
     where: {
       userId: user.id,
@@ -77,51 +47,10 @@ export default async function budget() {
         totalExpense += budgetYear[i].expenses[j].cost;
       }
     }
->>>>>>> f453ce90d1a213d5ee8f42d8419701033f640f28
 
-		return totalExpense;
-	}
+    return totalExpense;
+  }
 
-<<<<<<< HEAD
-	const isSurplus = totalCapital(capital) - totalExpenses(expenses);
-
-	function checkSurplus(isSurplus) {
-		if (isSurplus >= 0) {
-			return true;
-		}
-		return false;
-	}
-
-	return (
-		<section className="budget-section">
-			<Link href={"/capital"}>
-				<div id="totalCapital" className="budget-item">
-					<h2>Capital</h2>
-					<p>{totalCapital(capital)}</p>
-				</div>
-			</Link>
-
-			<Link href={"/expense"}>
-				<div id="totalExpense" className="budget-item">
-					<h2>Expenses</h2>
-					<p>{totalExpenses(expenses)}</p>
-				</div>
-			</Link>
-
-			<div id="budgetAllocation" className="budget-allocation">
-				<h2
-					style={{
-						fontSize: "1.5em",
-						color: isSurplus >= 0 ? "#27ae60" : "#c0392b",
-					}}>
-					{checkSurplus(isSurplus) ? "Surplus" : "Deficit"}
-				</h2>
-				<p>{isSurplus}</p>
-	
-			</div>
-		</section>
-	);
-=======
   return (
     <section className="budget-section">
       {separatedArrays.map((budgetYear) => {
@@ -142,5 +71,4 @@ export default async function budget() {
       })}
     </section>
   );
->>>>>>> f453ce90d1a213d5ee8f42d8419701033f640f28
 }
