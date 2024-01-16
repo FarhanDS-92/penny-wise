d3d9facf31c8dd0eb6d5af68dab649c3fcbce1eb
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-01-16 13:43:30 -0500

    spacing

--- src/app/(pages)/budget/page.jsx
+++ src/app/(pages)/budget/page.jsx
@@ -4,78 +4,79 @@ import { prisma } from "@/lib/prisma.js";
 import Link from "next/link.js";
 import CreateBudget from "@/app/components/CreateBudget.jsx";
 export default async function budget() {
-	const user = await fetchUser();
-	const budgets = await prisma.budget.findMany({
-		where: {
-			userId: user.id,
-		},
-		include: {
-			expenses: true,
-		},
-		orderBy: [
-			{
-				year: "asc",
-			},
-			{
-				month: "asc",
-			},
-		],
-	});
+  const user = await fetchUser();
+  const budgets = await prisma.budget.findMany({
+    where: {
+      userId: user.id,
+    },
+    include: {
+      expenses: true,
+    },
+    orderBy: [
+      {
+        year: "asc",
+      },
+      {
+        month: "asc",
+      },
+    ],
+  });
 
-	// this separates the budget array to be split by years and group them ot its own array
-	const separatedArrays = [];
-	budgets.forEach((obj) => {
-		const year = obj.year;
+  // this separates the budget array to be split by years and group them ot its own array
+  const separatedArrays = [];
+  budgets.forEach((obj) => {
+    const year = obj.year;
 
-		const index = separatedArrays.findIndex((arr) => arr[0].year === year);
+    const index = separatedArrays.findIndex((arr) => arr[0].year === year);
 
-		if (index === -1) {
-			separatedArrays.push([obj]);
-		} else {
-			separatedArrays[index].push(obj);
-		}
-	});
+    if (index === -1) {
+      separatedArrays.push([obj]);
+    } else {
+      separatedArrays[index].push(obj);
+    }
+  });
 
-	// console.log(separatedArrays[0]);
+  // console.log(separatedArrays[0]);
 
-	function yearlyExpense(budgetYear) {
-		let totalExpense = 0;
+  function yearlyExpense(budgetYear) {
+    let totalExpense = 0;
 
-		for (let i = 0; i < budgetYear.length; i++) {
-			for (let j = 0; j < budgetYear[i].expenses.length; j++) {
-				totalExpense += budgetYear[i].expenses[j].cost;
-			}
-		}
+    for (let i = 0; i < budgetYear.length; i++) {
+      for (let j = 0; j < budgetYear[i].expenses.length; j++) {
+        totalExpense += budgetYear[i].expenses[j].cost;
+      }
+    }
 
-		return totalExpense;
-	}
+    return totalExpense;
+  }
 
-	return (
-		<div>
-			<CreateBudget />
-			<section className="budget-section">
-				{separatedArrays.map((budgetYear) => {
-					return (
-						<div key={budgetYear[0].year}>
-							<div className="budgetYear">
-								<h2>{budgetYear[0].year}</h2>
-								<h2>Yearly Expense: ${yearlyExpense(budgetYear)}</h2>
-							</div>
-							<div className="monthList">
-								{budgetYear.map((budgetMonth) => {
-									return (
-										<Link
-											key={budgetMonth.id}
-											href={`/budget/${budgetMonth.id}`}>
-											<BudgetLink budget={budgetMonth} />
-										</Link>
-									);
-								})}
-							</div>
-						</div>
-					);
-				})}
-			</section>
-		</div>
-	);
+  return (
+    <div>
+      <CreateBudget />
+      <section className="budget-section">
+        {separatedArrays.map((budgetYear) => {
+          return (
+            <div key={budgetYear[0].year}>
+              <div className="budgetYear">
+                <h2>{budgetYear[0].year}</h2>
+                <h2>Yearly Expense: ${yearlyExpense(budgetYear)}</h2>
+              </div>
+              <div className="monthList">
+                {budgetYear.map((budgetMonth) => {
+                  return (
+                    <Link
+                      key={budgetMonth.id}
+                      href={`/budget/${budgetMonth.id}`}
+                    >
+                      <BudgetLink budget={budgetMonth} />
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
