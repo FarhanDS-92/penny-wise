7fd01df2ebe92e07ae80942a6708547cbdfd5c41
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-01-24 14:57:57 -0500

    small fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,17 +4,17 @@ import "./globals.css";
 import Footer from "./components/Footer.jsx";
 
 export default async function RootLayout({ children }) {
-	const user = await fetchUser();
+  const user = await fetchUser();
 
-	return (
-		<html lang="en" id={user.isDarkMode ? "html-dark" : "html-light"}>
-			<body>
-				<div className="page-container">
-					<NavBar user={user} />
-					<main>{children}</main>
-					<Footer />
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" id={user.isDarkMode ? "html-dark" : "html-light"}>
+      <body>
+        <NavBar user={user} />
+        <div className="page-container">
+          <main>{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
 }
