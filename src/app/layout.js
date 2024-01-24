7b19d02b8d1b7cf817115dd20414d008edffc76c
pimpa pongsopa <you@example.compp-psp@hotmail>
2024-01-24 10:37:05 -0800

    footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
 import { fetchUser } from "@/lib/fetchUser.js";
 import NavBar from "./components/NavBar.jsx";
 import "./globals.css";
+import Footer from "./components/Footer.jsx";
 
 export default async function RootLayout({ children }) {
-  const user = await fetchUser();
+	const user = await fetchUser();
 
-  return (
-    <html lang="en">
-      <body>
-        <NavBar user={user} />
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<div className="page-container">
+					<NavBar user={user} />
+					<main>{children}</main>
+					<Footer />
+				</div>
+			</body>
+		</html>
+	);
 }
